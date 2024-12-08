9470c6f0a2243a281c813b00417d6b9e143e4833
samuelxt18 <samuelsantos0576@gmail.com>
2024-12-08 14:14:45 -0300

    entrega mensagens e gravando no mongo!!

--- index.js
+++ index.js
@@ -1,65 +1,110 @@
-/*imports dos pacotes :
--express
--ejs
--http
--path
--socket.io */
 
+//back 
+/*
+    import dos pacotes:
+    -express
+    -ejs
+    -http
+    -path
+    -socket
+*/
 const express = require('express');
-const ejs = require('ejs');
+const ejs=require('ejs');
 const http = require('http');
-const path = require('path');
+const path= require('path');
 const socketIO = require('socket.io');
+const mongoose = require('mongoose');
 
-/*instançias do express
--express
--server
--socketIO */
+/* 
+
+    instancias 
+*/
 const app = express();
 const server = http.createServer(app);
-const io =socketIO(server);
-
+const io = socketIO(server);
+/* Importa o pacote do mongoose: */
 /*
-define a pasta estatica
+define a localização da pasta estatica 
 */
-app.use(express.static(path.join))(__dirname,'public')
+app.use(express.static(path.join(__dirname, 'public')));//acha o caminho estaticos 
 
-app.set('views',path.join(__dirname,'public'))
-app.engine('html',ejs.renderFile)
+/* 
+    define o ejs como a chave de inicialização 
+*/
+// app.set('view engine', 'html')
+app.set('views',path.join(__dirname, 'public'));
+app.engine('html', ejs.renderFile);
 
-/* rota raiz '/' para acessar o index da aplicação*/ 
 
 app.use('/', (req,res) =>{
-    res.render('index.html');
+    res.sendFile(path.join( 'index.html'));
 });
 
-/* inicio do cod do chat */
+/*Conexão com o MongoDB: */
+function connectDB() {
 
-/* array que armazena as mensagens */
-let messages =[];
+    /* URL de conexão com o Atlas mongoDB: */
+    let dbUrl = 'mongodb+srv://samuelxtp:QXjioa39ZVi5ihgb@cluster0.ro3opzs.mongodb.net/';
 
-/*cria a conexão com socket io*/
+    mongoose.connect(dbUrl);
+    mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
+    mongoose.connection.once('open', function callback(){
+        console.log("Atlas mongoDB conectado!");
+    });
 
+}
+/* Chama a função de conexão com o banco de dados */
+connectDB();
+
+/* Define o model */
+let Message = mongoose.model('Message',{ author : String, data_hora : String, message : String});
+
+//armazena msgs 
+
+let messages=[];
+
+/*Recupera as mensagens do banco de dados: */
+Message.find({})
+    .then(docs=>{
+        console.log('DOCS: ' + docs);
+        messages = docs;
+        console.log('MESSAGES: ' + messages);
+    }).catch(err=>{
+        console.log(err);
+    });
+
+// cria conection com o socket.io
 io.on('connection', socket=>{
-    console.log('novo usuario conectado! id: ' + socket.id)
-    /*RECUPÉRA E MANTER AS MENSAGENS DO FRONT ENDPARA O BACK*/
-    socket.emit('PreviousMessage', messages)
-    /* DISPARA AÇOES QND RECEBE AS ME  MENSAGENS DO FRONT*/
+
+    /* Exibe a título de teste da conexão o id do socket do usuário conectado: */
+    console.log(`Novo usuário conectado ${socket.id}`);
+
+    /* Recupera e mantem as mensagens do front para back e vice-versa: */
+    socket.emit('previousMessage', messages);
+
+    /* Dispara ações quando recebe mensagens do frontend: */
     socket.on('sendMessage', data => {
-        /* adiciona a nova mensagem para todos os*/
-        messages.push(data)
 
-        /*Propaga a mensagem para todos os usuarios conectados no*/
-        socket.broadcast.emit('receivedMessage',data);
+    /* Adicona uma mensagem enviada no final do array de mensagens: */
+    // messages.push(data);
+    let message = new Message(data);
+    message.save()
+        .then(
+            socket.broadcast.emit('receivedMessage', data)
+        )
+        .catch(err=>{
+            console.log('ERRO: ' + err);
+        });
 
-    })
-    /* responsaveS A*/
-});
-/*fim do cod do chat */
-/*
-Criação do servidor http
+    /* Propaga a mensagem enviada para todos os usuário conectados na aplicaçao de chat: */
+    // socket.broadcast.emit('receivedMessage', data);
+
+    });
+
+})
+/* 
+    criação server http 
 */
-
-server.listen(3000, ()=> {
-    console.log('Servidor do web chat rodando em -> http://localhost:3000')
+server.listen(3000, () =>{
+    console.log('server funcionando no http://localhost:3000')
 });
